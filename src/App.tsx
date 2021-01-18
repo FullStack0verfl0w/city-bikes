1b99b17029d7fb645ca28f501e38a6a5024bd898
pBinarySoul <pBinarySoul@gmail.com>
2021-01-18 20:55:08 +0300

    Project template

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FC } from 'react';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+type TAppProps = {};
+const App: FC<TAppProps> = (props) => {
+
+    return (
+        <div className="app-container">
+                <div className="app-container-top">
+                    <h1 className="app-container-top__title">{
+                        "Выберите сеть"
+                    }</h1>
+                    <h2 className="app-container-top__stations">{
+                        ""
+                    }</h2>
+                </div>
+                <div className="app-container-bottom">
+                    {/*Сюда засунуть списки*/}
+                </div>
+        </div>
+    );
+};
 
 export default App;
